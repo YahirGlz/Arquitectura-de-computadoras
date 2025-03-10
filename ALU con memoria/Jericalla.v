b4d45c5a361ebb376f6a89ab251c84329a3be8c0
YahirGlz <155937398+YahirGlz@users.noreply.github.com>
2025-03-10 10:10:13 -0600

    Add files via upload
    10 de marzo del 2025

---
+++ ALU con memoria/Jericalla.v
@@ -0,0 +1,3 @@
+module Jericalla(input [16:0], output reg [31:0]dataOut);
+
+endmodule 
---
+++ ALU con memoria/Operaciones.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ns
+
+module AND(input [31:0]A, input [31:0]B, output reg [31:0]R);
+  assign R = A&B;
+endmodule
+
+module OR(input [31:0]A, input [31:0]B, output reg [31:0]R);
+  assign R = A|B;
+endmodule
+
+module ADD(input [31:0]A, input [31:0]B, output reg [31:0]R);
+  assign R = A+B;
+endmodule
+
+module SUBS(input [31:0]A, input [31:0]B, output reg [31:0]R);
+  assign R = A-B;
+endmodule
+
+module SOLT(input [31:0]A, input [31:0]B, output reg [31:0]R);
+  assign R = A<B?1:0;
+endmodule
+
+module NOR(input [31:0]A, input [31:0]B, output reg [31:0]R);
+  assign R = ~(A|B);
+endmodule
+
+
+
+
+
+
