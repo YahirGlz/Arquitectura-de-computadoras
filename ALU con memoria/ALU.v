118a412de396fb88f6934f92d26f56ee25308e6e
YahirGlz <155937398+YahirGlz@users.noreply.github.com>
2025-03-12 10:32:35 -0600

    Update ALU.v

--- ALU con memoria/ALU.v
+++ ALU con memoria/ALU.v
@@ -1,4 +1,4 @@
-module ALU_SIN_BLOQUEO(input [31:0]X, input [31:0]Y, input [3:0]S, output reg [31:0]r, output reg Zflag);
+module ALU(input [31:0]X, input [31:0]Y, input [3:0]S, output reg [31:0]r, output reg Zflag);
   wire [31:0]w[5:0];
   AND  _and (.A(X), .B(Y), .R(w[0]));
   OR   _or  (.A(X), .B(Y), .R(w[1]));
@@ -7,7 +7,7 @@ module ALU_SIN_BLOQUEO(input [31:0]X, input [31:0]Y, input [3:0]S, output reg [3
   SOLT _solt(.A(X), .B(Y), .R(w[4]));
   NOR  _nor (.A(X), .B(Y), .R(w[5]));
   always @(*) begin
-    case (s)
+    case (S)
 	  3'd0:  begin r = w[0]; end 
 	  3'd1:  begin r = w[1]; end 
 	  3'd2:  begin r = w[2]; end 
